d60d23f9b9b0baa93286eabb1382c250558bcc6d
AngryPixel <lokesh.dawar@gmail.com>
2023-12-31 00:29:02 -0500

    Updated example one

--- examples/one/localFile.tf
+++ examples/one/localFile.tf
@@ -1,9 +1,14 @@
+# Create
 # resource "local_file" "pet" {
 #   filename = "/home/null/work/projects/terraform/examples/one/pets.txt"
 #   content = "Hello World  .... Pet !!!"
 # }
 
+# Update (plan and apply)
 resource "local_file" "pet" {
   filename = "/home/null/work/projects/terraform/examples/one/pets.txt"
   content = "Hello World  Again .... Pet !!!"
 }
+
+# Delete tf destroy delete all the resources in plan
+
